f0498cc0433b5dbf1959dd58e1a84732935075ec
Viktor <victor318x@gmail.com>
2019-01-19 01:54:09 +0800

    fix: 修复配置脚本

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:/ViktorWong/Front-end-Library.git master:gh-pages
+git push -f git@github.com:ViktorWong/Front-end-Library.git master:gh-pages
 
 cd -
