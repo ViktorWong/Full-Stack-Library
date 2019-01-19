7d17299933a958eaa1f310b8dcbfcdaf54a34240
Viktor <victor318x@gmail.com>
2019-01-19 13:15:16 +0800

    feat: 添加库、关于我们栏目

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,59 +2,67 @@
  * --- 配置文件的入口文件 ---
  */
 
-const utils = require("./utils")
+const utils = require('./utils')
 
 module.exports = {
-  title: "Front-end Library",
-  description: "前端图书馆",
-  base: "/Front-end-Library/",
+  title: 'Front-end Library',
+  description: '前端图书馆',
+  base: '/Front-end-Library/',
   head: [
     [
-      "link",
+      'link',
       {
-        rel: "icon",
-        href: "/favicon.ico"
+        rel: 'icon',
+        href: '/favicon.ico'
       }
     ]
   ],
   themeConfig: {
     nav: [
       {
-        text: "首页",
-        link: "/"
+        text: '首页',
+        link: '/'
       },
       {
-        text: "文章",
-        link: "/article/"
+        text: '库',
+        link: '/repository/'
       },
       {
-        text: "面试",
-        link: "/interview/"
+        text: '文章',
+        link: '/article/'
       },
       {
-        text: "Blog",
-        link: "/blog/"
+        text: '面试',
+        link: '/interview/'
+      },
+      {
+        text: '关于我们',
+        link: '/about/'
+      },
+      {
+        text: 'Blog',
+        link: 'https://viktorwong.github.io/'
       }
     ],
     sidebar: utils.inferSiderbars(),
-    lastUpdated: "上次更新",
-    repo: "ViktorWong/Front-end-Library",
+    lastUpdated: '上次更新',
+    repo: 'ViktorWong/Front-end-Library',
     editLinks: true,
-    docsDir: "docs",
-    editLinkText: "在 GitHub 上编辑此页",
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
     sidebarDepth: 3
   },
   configureWebpack: {
     resolve: {
       alias: {
-        "@public": "./public"
+        '@public': './public'
       }
     }
   },
   markdown: {
     config: md => {
       // use more markdown-it plugins!
-      md.use(require("markdown-it-include"))
+      md.use(require('markdown-it-include'))
     }
   }
 }
